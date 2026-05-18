7da5daadf1c8817ab14547de5ba113062eced0ab
sujitfalia-blip <sujitfalia@gmail.com>
2026-05-18 21:32:30 +0530

    Update __init__.py

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,1 +1,15 @@
+from routes.auth import auth_bp
+from routes.wallet import wallet_bp
+from routes.game import game_bp
 
+from routes.betting import betting_bp
+from routes.sideshow import sideshow_bp
+from routes.showdown import showdown_bp
+from routes.winner import winner_bp
+from routes.timer import timer_bp
+from routes.turn import turn_bp
+from routes.history import history_bp
+
+from routes.transaction import transaction_bp
+from routes.commission import commission_bp
+from routes.monitoring import monitoring_bp
